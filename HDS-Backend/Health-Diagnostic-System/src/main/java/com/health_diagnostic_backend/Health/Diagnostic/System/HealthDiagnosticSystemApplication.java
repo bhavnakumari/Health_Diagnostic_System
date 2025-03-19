01add8d8f687ce579746a1cf0421bb52bef20580
bhavnakumari <bhavnagupta0103@gmail.com>
2025-03-19 19:27:44 -0400

    first push

---
+++ HDS-Backend/Health-Diagnostic-System/src/main/java/com/health_diagnostic_backend/Health/Diagnostic/System/HealthDiagnosticSystemApplication.java
@@ -0,0 +1,13 @@
+package com.health_diagnostic_backend.Health.Diagnostic.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDiagnosticSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDiagnosticSystemApplication.class, args);
+	}
+
+}
---
+++ HDS-Backend/Health-Diagnostic-System/src/test/java/com/health_diagnostic_backend/Health/Diagnostic/System/HealthDiagnosticSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health_diagnostic_backend.Health.Diagnostic.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDiagnosticSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
